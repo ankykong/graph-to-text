c56d8a0b664a2cf4b353c9a9ff9964ef9ac27fd4
Ankur Duggal <38927181+ankykong@users.noreply.github.com>
2024-11-16 23:55:22 +0000

    Removing Datasets

--- evaluation_metrics/bleurt/bleurt/test_data/average_scores.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-with open("statista/c2t_data/scores", 'r', encoding='utf-8') as actualfile:
-            scores = actualfile.readlines()
-
-
-print(sum([float(i[:-1]) for i in scores])/len(scores))
